140c188a1b6ea3078089fb4d93444dfe9e1233bd
𝕄𝕣.𝔸𝕝𝕡𝕖𝕣𝕥 <157471867+MrAlpert@users.noreply.github.com>
2025-05-25 14:47:37 +0300

    RithmicNewMarketData.cs

---
+++ RithmicApi/RithmicNewMarketData.cs
@@ -0,0 +1,11 @@
+namespace RithmicApi
+{
+	public struct RithmicNewMarketData
+	{
+		public double Ask { get; set; }
+
+		public double Bid { get; set; }
+
+		public string Symbol { get; set; }
+	}
+}
