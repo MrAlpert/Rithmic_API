14ca3f60eb7c8a5de3dee7052612e1e6b6eeff83
𝕄𝕣.𝔸𝕝𝕡𝕖𝕣𝕥 <157471867+MrAlpert@users.noreply.github.com>
2025-05-25 14:46:02 +0300

    AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("RithmicApi")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("RithmicApi")]
+[assembly: AssemblyTitle("RithmicApi")]
+[assembly: AssemblyVersion("1.0.0.0")]
