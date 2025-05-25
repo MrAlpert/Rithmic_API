4d73cebf8f1bb03fdc694522f7082e9124cdf775
𝕄𝕣.𝔸𝕝𝕡𝕖𝕣𝕥 <157471867+MrAlpert@users.noreply.github.com>
2025-05-25 14:50:07 +0300

    DataRithmicServer.cs

---
+++ RithmicApi.Models/DataRithmicServer.cs
@@ -0,0 +1,35 @@
+namespace RithmicApi.Models
+{
+	public class DataRithmicServer
+	{
+		public string Name;
+
+		public string AdmCnnctPt;
+
+		public string AppName;
+
+		public string AppVersion;
+
+		public string CertFile;
+
+		public string LogFilePath;
+
+		public string DmnSrvrAddr;
+
+		public string DomainName;
+
+		public string LicSrvrAddr;
+
+		public string LocBrokAddr;
+
+		public string LoggerAddr;
+
+		public string MdConnectionPoint;
+
+		public string TsConnectionPoint;
+
+		public string PnlConnectionPoint;
+
+		public string HistorycConnectionPoint;
+	}
+}
