2b8d2c56ed028eeef0e91c02928cb001e6f0fe98
𝕄𝕣.𝔸𝕝𝕡𝕖𝕣𝕥 <157471867+MrAlpert@users.noreply.github.com>
2025-05-25 14:49:28 +0300

    RithmicServerType.cs

---
+++ RithmicApi.Enums/RithmicServerType.cs
@@ -0,0 +1,13 @@
+namespace RithmicApi.Enums
+{
+	public enum RithmicServerType
+	{
+		Rithmic_01,
+		Rithmic_Demo,
+		Rithmic_Germany,
+		Rithmic_Demo_singapore,
+		Rithmic_Demo_Hong_Kong,
+		Rithmic_Demo_Chicago_Area,
+		Rithmic_Demo_Chicago_Area2
+	}
+}
